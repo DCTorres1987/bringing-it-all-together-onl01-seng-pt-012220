4fa65ec80765d07e27c30a6bc314f0690edd5569
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-04 17:35:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,10 +46,10 @@ class Dog
       self
   end
   
-  def self.create(name, breed)
+  def self.create(name:, breed:)
     song = Dog.new(name, breed)
     song.save
-    song
+    dog
   end
   
 end
